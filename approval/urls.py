589a97d4162774493f7e6c116339be8d256e9ed6
tomoyanakamura <tomoya@tomoyanakamuranoMacBook-Air.local>
2024-11-12 20:43:53 +0900

    メッセージ

--- approval/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('approve/<int:attendance_id>/', views.approve_attendance, name='approve_attendance'),
-    path('resubmit/<int:attendance_id>/', views.resubmit_attendance, name='resubmit_attendance'),
-]
